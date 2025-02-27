b74c0a4113522f955c9cf9e291b9a1e30f03784f
rizkitriananda <riskitriananda321@gmail.com>
2025-02-27 16:14:25 +0700

    Fix mixed content errors

--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login({ status, canResetPassword }) {
         e.preventDefault();
 
         try {
-            const response = await fetch("api/login", {
+            const response = await fetch("/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:8000/api/users", {
+            const response = await fetch("/api/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
